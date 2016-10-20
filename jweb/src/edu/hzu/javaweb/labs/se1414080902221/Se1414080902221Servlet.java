c67252ac149e7899c01cd4a013bd9e32f922af90
ChowWd <1297422578@qq.com>
2016-10-20 23:28:06 +0800

    #4 提交实验代码

---
+++ jweb/src/edu/hzu/javaweb/labs/se1414080902221/Se1414080902221Servlet.java
@@ -0,0 +1,28 @@
+package edu.hzu.javaweb.labs.se1414080902221;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+//import javax.xml.ws.WebServiceRefs;
+@WebServlet("/1414080902221")
+public class Se1414080902221Servlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request,response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("Success");
+	}
+
+}
