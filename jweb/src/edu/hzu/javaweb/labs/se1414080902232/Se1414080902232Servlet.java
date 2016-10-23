0e6a9d040e9db31d6c344d0cfc67a316f19e41ea
Cai <754181906@qq.com>
2016-10-23 22:53:05 +0800

    ..

--- jweb/src/edu/hzu/javaweb/labs/se1414080902232/Se1414080902232Servlet.java
+++ jweb/src/edu/hzu/javaweb/labs/se1414080902232/Se1414080902232Servlet.java
@@ -7,7 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+@WebServlet("/1414080902232")
 public class Se1414080902232Servlet extends HttpServlet {
 
 	/**
