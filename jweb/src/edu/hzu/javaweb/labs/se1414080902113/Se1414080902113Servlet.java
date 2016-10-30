bfa945ce6576a4c5994623ffbde5b52ff7b308ab
unknown <>
2016-10-30 20:57:57 +0800

    #4 提交实验代码

--- jweb/src/edu/hzu/javaweb/labs/se1414080902113/Se1414080902113Servlet.java
+++ jweb/src/edu/hzu/javaweb/labs/se1414080902113/Se1414080902113Servlet.java
@@ -1,92 +1,48 @@
+package edu.hzu.javaweb.labs.se1414080902113;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-
+@WebServlet("/1414080902113")
 public class Se1414080902113Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 
-	/**
-	 * Constructor of the object.
-	 */
 	public Se1414080902113Servlet() {
 		super();
 	}
 
-	/**
-	 * Destruction of the servlet. <br>
-	 */
+	public void init(ServletConfig config) throws ServletException {
+		
+	}
+
 	public void destroy() {
-		super.destroy(); // Just puts "destroy" string in log
-		// Put your code here
 	}
 
-	/**
-	 * The doGet method of the servlet. <br>
-	 *
-	 * This method is called when a form has its tag value method equals to get.
-	 * 
-	 * @param request the request send by the client to the server
-	 * @param response the response send by the server to the client
-	 * @throws ServletException if an error occurred
-	 * @throws IOException if an error occurred
-	 */
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
-		out.println("<HTML>");
-		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
-		out.println("  <BODY>");
-		out.print("    This is ");
-		out.print(this.getClass());
-		out.println(", using the GET method");
-		out.println("  </BODY>");
-		out.println("</HTML>");
-		out.flush();
-		out.close();
+	       PrintWriter out=response.getWriter();
+	       response.setContentType("text/html;charset=utf-8");
+	       out.println("<HTML>");
+	       out.println("  <HEAD><TITLE>第1个Servlet示例</TITLE></HEAD>");
+	       out.println("<BODY>");
+	       out.println("测试成功!");
+	       out.println("</BODY>");
+	       out.println("</HTLM>");
+	       out.flush();
+	       out.close();
 	}
 
-	/**
-	 * The doPost method of the servlet. <br>
-	 *
-	 * This method is called when a form has its tag value method equals to post.
-	 * 
-	 * @param request the request send by the client to the server
-	 * @param response the response send by the server to the client
-	 * @throws ServletException if an error occurred
-	 * @throws IOException if an error occurred
-	 */
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
-		out.println("<HTML>");
-		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
-		out.println("  <BODY>");
-		out.print("    This is ");
-		out.print(this.getClass());
-		out.println(", using the POST method");
-		out.println("  </BODY>");
-		out.println("</HTML>");
-		out.flush();
-		out.close();
-	}
-
-	/**
-	 * Initialization of the servlet. <br>
-	 *
-	 * @throws ServletException if an error occurs
-	 */
-	public void init() throws ServletException {
-		// Put your code here
+		doGet(request, response);
 	}
 
 }
