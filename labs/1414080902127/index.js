96b6eb345f8dd7de4eb17325993bd98ecb096e99
unknown <>
2016-11-01 19:27:36 +0800

    #2 提交实验代码

--- labs/1414080902127/index.js
+++ labs/1414080902127/index.js
@@ -1,7 +1,8 @@
 $( "#target" ).submit(function( event ) {
   alert( "Handler for .submit() called." );
   event.preventDefault();
-});
+}); 
+//下列是录入成功
 $.ajax({
   url: "success.json
 }).done(function(data) {
