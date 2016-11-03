0a6f61fef61c308b438ee343a09cead7d598cb48
ch135 <707347865@qq.com>
2016-11-03 10:49:07 +0800

    #4 更改错误

--- jweb/src/edu/hzu/javaweb/labs/se1414080902135/Se135.java
+++ jweb/src/edu/hzu/javaweb/labs/se1414080902135/Se135.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import Model.Build;
-
+@WebServlet("/1414080902135")
 public class Se135 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws 
--- jweb/src/edu/hzu/javaweb/labs/se1414080902135/User_Session.java
+++ jweb/src/edu/hzu/javaweb/labs/se1414080902135/User_Session.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import Model.Build;
-
+@WebServlet("/User_Session")
 public class User_Session extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws 
