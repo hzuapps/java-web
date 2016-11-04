93e8ec2d8609c314d15a7a99fd5842c4e90ae686
pigosy <892365482@qq.com>
2016-11-04 17:05:23 +0800

    #5 提交实验4

---
+++ jweb/src/edu/hzu/javaweb/labs/se1414080902231/EncodingFilter.java
@@ -0,0 +1,34 @@
+package edu.hzu.javaweb.labs.se1414080902231;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter(filterName = "EncodingFilter",urlPatterns="/*")
+public class EncodingFilter implements Filter {
+
+    public EncodingFilter() {
+    }
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
