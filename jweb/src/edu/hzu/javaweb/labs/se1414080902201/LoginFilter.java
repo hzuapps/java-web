6cea6462c9a8e9af0e7fe96a9d6b456bb40ca4d4
Sm1les <1024835308@qq.com>
2016-11-07 00:28:45 +0800

    #5 提交实验代码

---
+++ jweb/src/edu/hzu/javaweb/labs/se1414080902201/LoginFilter.java
@@ -0,0 +1,58 @@
+package edu.hzu.javaweb.labs.se1414080902201;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class LoginFilter
+ */
+@WebFilter(filterName="LoginFilter",urlPatterns="/1414080902201")
+public class LoginFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public LoginFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		//获取http请求相关数据
+		HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        
+        HttpSession session = req.getSession();
+        if (session.getAttribute("username") == null) {
+        	resp.sendRedirect("login.jsp");
+        } else {   
+        	chain.doFilter(request, response);
+        }  
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		System.out.println("登录过滤...");
+	}
+
+}
