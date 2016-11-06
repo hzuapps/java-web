064443cd9ae8c83bf1e33270b1202d49ca112e04
heymanheymanheyman <747073320@qq.com>
2016-11-06 22:34:49 +0800

    #5 #95 提交实验代码

--- jweb/src/edu/hzu/javaweb/labs/se1414080902204/Login.java
+++ jweb/src/edu/hzu/javaweb/labs/se1414080902204/Login.java
@@ -1,15 +1,11 @@
 package edu.hzu.javaweb.labs.se1414080902204;
 
-import java.awt.Window;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-import javax.naming.spi.DirStateFactory.Result;
-import javax.security.auth.message.callback.PrivateKeyCallback.Request;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,16 +14,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class Se1414080902204Servlet
+ * Servlet implementation class Login
  */
-@WebServlet("/Se1414080902204Servlet")
-public class Se1414080902204Servlet extends HttpServlet {
+@WebServlet("/Login")
+public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Se1414080902204Servlet() {
+    public Login() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -37,53 +33,37 @@ public class Se1414080902204Servlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
 		response.setContentType("text/html;charset=utf-8");
 		String Name="com.mysql.jdbc.Driver";
         String userName="root";
         String userPwd="1234";
         String dbName="shenzhentong_system";
         String url="jdbc:mysql://localhost:3306/shenzhentong_system?user=root&password=1234&useUnicode=true&characterEncoding=UTF-8";
-
-      
-       String xingming= request.getParameter("Username");
-       String idCard =request.getParameter("IdCard");
-       String cardNumber =request.getParameter("CardNumber");
-       String phoneNumber=request.getParameter("Pnumber");
-       
+       String cardNumber =request.getParameter("kahao");
+       String phoneNumber=request.getParameter("shoujihao");
        RequestDispatcher dis=null;
        try{
     	   Class.forName(Name);
            Connection conn=DriverManager.getConnection(url);
-    	   String sql="select * from szt_information where(name=? or idcard=? or card=? or phonenumber=?)";
+    	   String sql="select * from szt_information where( card=? and phonenumber=?)";
     	   PreparedStatement pstmt=conn.prepareStatement(sql);
-    	   pstmt.setString(1,xingming);
-    	   pstmt.setString(2, idCard);
-    	   pstmt.setString(3, cardNumber);
-    	   pstmt.setString(4, phoneNumber);
+    	   pstmt.setString(1, cardNumber);
+    	   pstmt.setString(2, phoneNumber);
     	   ResultSet rs =pstmt.executeQuery();
     	   if (rs.next()){
     		   if(rs!=null) rs.close();
+    		   if(pstmt!=null) pstmt.close();
     		   if(conn!=null) conn.close();
-    		   dis=request.getRequestDispatcher("bangdingerro.jsp");
+    		   dis=request.getRequestDispatcher("gongneng.jsp");
     		   dis.forward(request, response);
     	   }
     	   
-    	   else{
-    		   String sql1="Insert into szt_information (name,idcard,card,phonenumber) values(?,?,?,?)";
-    		   PreparedStatement pstmt1=conn.prepareStatement(sql1);
-    		   request.setCharacterEncoding("UTF-8");
-    		   pstmt1.setString(1, xingming);
-    		   pstmt1.setString(2, idCard);
-    		   pstmt1.setString(3, cardNumber);
-    		   pstmt1.setString(4, phoneNumber);
-    		   int n=pstmt1.executeUpdate();
-    		   {  if(n==1){ System.out.println("数据插入数据库成功！");}
-    		   else {System.out.println("数据插入数据库失败！");}
-    		   }
-/*    		   if(rs!=null) rs.close();
-*/    		   if(pstmt!=null) pstmt.close();
+    	   else{	  
+    		   if(rs!=null) rs.close();
+    		   if(pstmt!=null) pstmt.close();
     		   if(conn!=null) conn.close();
-    		   dis=request.getRequestDispatcher("bangdingsuccess.jsp");
+    		   dis=request.getRequestDispatcher("shibai.jsp");
     		   dis.forward(request, response);
     		   
     	   }
@@ -97,12 +77,10 @@ public class Se1414080902204Servlet extends HttpServlet {
        
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		
 	}
 
 }
