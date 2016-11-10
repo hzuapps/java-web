d68a2f3bf817e0cf2986b43cf8d155d119d515e4
unknown <454552677@qq.com>
2016-11-10 20:20:47 +0800

    #4 #160 提交实验代码

---
+++ jweb/src/edu/hzu/javaweb/labs/se1414080902102/Se1414080902102Servlet.java
@@ -0,0 +1,80 @@
+package se1414080902102;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Se1414080902102Servlet extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public Se1414080902102Servlet() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doPost(request,response);
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String username=request.getParameter("username");
+		String password=request.getParameter("password");
+		String cat=request.getParameter("whichcat");
+		String info="";
+		if(cat!=null){
+			info=username+"已申请租车。车型为"+cat;
+		}
+		else{
+			info=username+"申请租车失败，请重新租车";
+		}
+		request.setAttribute("output", info);
+		request.getRequestDispatcher("/index.jsp").forward(request, response);
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
