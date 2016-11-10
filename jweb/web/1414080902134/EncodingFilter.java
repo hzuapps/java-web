2fab67196275bd5d88b22ee6635dfb2e23d3ea0a
Ibas Sun <Ibas Sun>
2016-11-10 13:49:39 +0800

    #104 sys4

---
+++ jweb/web/1414080902134/EncodingFilter.java
@@ -0,0 +1,53 @@
+package edu.hzu.javaweb.labs.se1414080902134;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class EncodingFilter
+ */
+@WebFilter(filterName = "/EncodingFilter",urlPatterns = "/*")
+public class EncodingFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public EncodingFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		String encoding = "UTF-8";
+        request.setCharacterEncoding(encoding);// 在请求里设置上指定的编码  
+        response.setCharacterEncoding(encoding);
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
