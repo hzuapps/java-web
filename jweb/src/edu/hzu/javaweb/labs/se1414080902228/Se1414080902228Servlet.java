764905af23da02c995153881e0a4390d2fb3d268
CaiRencai <caijet@qq.com>
2016-11-10 11:56:15 +0800

    #6 JiPiGeDa

--- jweb/src/edu/hzu/javaweb/labs/se1414080902228/Se1414080902228Servlet.java
+++ jweb/src/edu/hzu/javaweb/labs/se1414080902228/Se1414080902228Servlet.java
@@ -2,12 +2,16 @@ package edu.hzu.javaweb.labs.se1414080902228;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.mail.Session;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class Se1414080902228Servlet extends HttpServlet {
 
@@ -38,20 +42,29 @@ public class Se1414080902228Servlet extends HttpServlet {
 	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		System.out.println(new Date().toString());
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
-		out.println("<HTML>");
-		out.println("  <HEAD><TITLE>A Servlet</TITLE></HEAD>");
-		out.println("  <BODY>");
-		out.print("    This is ");
-		out.print(this.getClass());
-		out.println(", using the GET method");
-		out.println("  </BODY>");
-		out.println("</HTML>");
-		out.flush();
-		out.close();
+		
+		String email = request.getParameter("email");
+		HttpSession session = request.getSession();
+		session.setAttribute("email", email);
+		
+		List<Product> products = new ArrayList<Product>();
+		String[] name = new String[]{"Æ»¹û","Àæ","Ïã½¶","Î÷¹Ï","ÁñÁ«","Ë®ÃÛÌÒ"};
+		Date date = new Date();
+		Product product;
+		for(int i = 0; i < 6; i++){
+			product = new Product();
+			product.setDate(date.getYear() + "-" + date.getMonth() + "-" + date.getDay());
+			product.setId(i+"");
+			product.setName(name[i]);
+			product.setNum(i);
+			product.setDescribe("ºÃ³Ô£¬ÐÂÏÊ");
+			product.setRemarks("ÎÞ");
+			product.setType(0);
+		}
+		
+		request.setAttribute("products", products);
+		request.getRequestDispatcher("/index").forward(request, response);
+		
 	}
 
 	/**
