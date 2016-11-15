0853212433d089c6d5803c712192ba4cae55e55a
OctRStar <792700864@qq.com>
2016-11-15 23:49:21 +0800

    #5 提交实验代码

---
+++ jweb/src/edu/hzu/javaweb/labs/se1414080902215/LoginFilter.java
@@ -0,0 +1,52 @@
+package edu.hzu.javaweb.labs.se1414080902215;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter(filterName = "LoginFilter", urlPatterns = "/*")
+
+public class LoginFilter implements Filter  {
+	
+	public void doFilter(ServletRequest request,   
+        ServletResponse response,   
+        FilterChain chain)   
+       throws IOException, ServletException {   
+ 
+    HttpServletRequest req = (HttpServletRequest) request;   
+    HttpServletResponse res = (HttpServletResponse) response;   
+ 
+    HttpSession session = req.getSession();   
+    	if (session.getAttribute("username") != null) {//登录后才能访问   
+        chain.doFilter(request, response);   
+    	} 
+    	else {   
+    			res.sendRedirect("../failure.jsp");   
+    		}   
+	}
+
+	
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	} 
+}
