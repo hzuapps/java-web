f682261ce3c2ecb4e6c87e2b5b54d5c9dbb6cfd0
Darkreunion <1002246995@qq.com>
2016-11-16 12:27:49 +0800

    #4 #5 提交实验代码

---
+++ jweb/src/edu/hzu/javaweb/labs/se1414080902239/LoginFilter.java
@@ -0,0 +1,46 @@
+package Filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request,   
+	         ServletResponse response,   
+	         FilterChain chain)   
+	        throws IOException, ServletException {   
+	  
+	     HttpServletRequest req = (HttpServletRequest) request;   
+	     HttpServletResponse res = (HttpServletResponse) response;   
+	  
+	     HttpSession session = req.getSession();   
+	    if (session.getAttribute("username") != null) {//登录后才能访问   
+	         chain.doFilter(request, response);   
+	     } else {   
+	         res.sendRedirect("failure.jsp");   
+	     }   
+	}  
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
