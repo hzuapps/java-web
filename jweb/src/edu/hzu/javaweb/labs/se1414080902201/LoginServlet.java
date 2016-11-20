f012c29ae38ce746027c1ba6456acad2e3a3586b
Sm1les <1024835308@qq.com>
2016-11-20 12:44:17 +0800

    #7 提交实验代码

--- jweb/src/edu/hzu/javaweb/labs/se1414080902201/LoginServlet.java
+++ jweb/src/edu/hzu/javaweb/labs/se1414080902201/LoginServlet.java
@@ -1,6 +1,13 @@
 package edu.hzu.javaweb.labs.se1414080902201;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.MessageDigest;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -16,7 +23,13 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name="LoginServlet",urlPatterns="/login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	// JDBC 驱动名及数据库 URL
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	static final String DB_URL = "jdbc:mysql://localhost:3306/todox";
+	
+	// 数据库的用户名与密码
+	static final String USER = "root";
+	static final String PASS = ""; 
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -36,6 +49,9 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("处理post请求...");
+		Connection conn = null;
+		Statement stmt = null;
+		String id = null;
 		// 接收数据
         String email = request.getParameter("email");
         String password = request.getParameter("password");
@@ -52,17 +68,74 @@ public class LoginServlet extends HttpServlet {
             dispatcher.forward(request, response);
             return;
         }
+        //连接数据库
+        try{
+			// 注册 JDBC 驱动器
+			Class.forName("com.mysql.jdbc.Driver");
+			// 打开一个连接
+			conn = DriverManager.getConnection(DB_URL,USER,PASS);
+			// 执行 SQL 查询
+			stmt = conn.createStatement();
+			String sql;
+			sql = "SELECT * FROM user WHERE email='"+email+"'";
+			ResultSet rs = stmt.executeQuery(sql);
+			if(rs.next()){
+				id = rs.getString("id");
+				String pass = rs.getString("password");
+				System.out.println(id);
+				if(!password.equals(pass)){
+					request.setAttribute("tip", "密码错误！");                                        
+		            RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");   
+		            dispatcher.forward(request, response);
+		            return;
+				}
+			}else{
+				request.setAttribute("tip", "该邮箱未注册！");                                        
+	            RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");   
+	            dispatcher.forward(request, response);
+	            return;
+			}
+			
+			// 完成后关闭
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch(SQLException se) {
+			// 处理 JDBC 错误
+			se.printStackTrace();
+		} catch(Exception e) {
+			// 处理 Class.forName 错误
+			e.printStackTrace();
+		}finally{
+			// 最后是用于关闭资源的块
+			try{
+				if(stmt!=null)
+				stmt.close();
+			}catch(SQLException se2){
+			}
+			try{
+				if(conn!=null)
+				conn.close();
+			}catch(SQLException se){
+				se.printStackTrace();
+			}
+		}
+
         //创建 Cookie
  		Cookie cookie_email = new Cookie("email",email);
+ 		Cookie cookie_id = new Cookie("id",id);
  		if(remember != null){
  			// 设置过期日期为30天后
  	 		cookie_email.setMaxAge(60*60*24*30);
+ 	 		cookie_id.setMaxAge(60*60*24*30);
  		}else{
  			//设置过期时间为1小时
  			cookie_email.setMaxAge(60*60);
+ 			cookie_id.setMaxAge(60*60);
  		}
  		// 在响应头中 Cookie
  		response.addCookie(cookie_email);
+ 		response.addCookie(cookie_id);
  		response.sendRedirect("index");
 
 
