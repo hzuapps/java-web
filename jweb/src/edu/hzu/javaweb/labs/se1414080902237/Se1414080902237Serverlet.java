c3aa6be9116f9cccb0ad00b7711840eff91ff20e
SlowSoulWen <664381601@qq.com>
2016-11-24 13:54:34 +0800

    #4 提交实验代码

---
+++ jweb/src/edu/hzu/javaweb/labs/se1414080902237/Se1414080902237Serverlet.java
@@ -0,0 +1,81 @@
+package edu.hzu.javaweb.labs.se1414080902237;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Se1414080902237Servlet extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public Se1414080902237Servlet() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("提交成功");
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE>");
+		out.println("<HTML>");
+		out.println("  <HEAD><meta http-equiv='Content-Type' content='text/html; charset=utf-8'/><TITLE>搜索结果</TITLE></HEAD>");
+		out.println("  <BODY>");
+		out.println("<h1>暂无内容！</h1>");
+		out.println("  </BODY>");
+		out.println("</HTML>");
+		out.flush();
+		out.close();
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
