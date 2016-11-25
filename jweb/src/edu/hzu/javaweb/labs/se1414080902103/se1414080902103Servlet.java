21593a04d515d56ea3adc54560ff9dee8fa9a948
hvvvvccc <hvvvvccc@126.com>
2016-11-26 02:12:52 +0800

    #6 提交实验报告

--- jweb/src/edu/hzu/javaweb/labs/se1414080902103/se1414080902103Servlet.java
+++ jweb/src/edu/hzu/javaweb/labs/se1414080902103/se1414080902103Servlet.java
@@ -2,14 +2,18 @@ package edu.hzu.javaweb.labs.se1414080902103;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
+
 @WebServlet("/1414080902103")
 public class Se1414080902103Servlet extends HttpServlet {
 
@@ -30,52 +34,76 @@ public class Se1414080902103Servlet extends HttpServlet {
 
 	/**
 	 * The doGet method of the servlet. <br>
-	 *
+	 * 
 	 * This method is called when a form has its tag value method equals to get.
 	 * 
-	 * @param request the request send by the client to the server
-	 * @param response the response send by the server to the client
-	 * @throws ServletException if an error occurred
-	 * @throws IOException if an error occurred
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
 	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		response.setContentType("text/html");
-	    PrintWriter out=response.getWriter();
+		PrintWriter out = response.getWriter();
 		out.print("1414080902103");
 	}
 
 	/**
 	 * The doPost method of the servlet. <br>
-	 *
-	 * This method is called when a form has its tag value method equals to post.
 	 * 
-	 * @param request the request send by the client to the server
-	 * @param response the response send by the server to the client
-	 * @throws ServletException if an error occurred
-	 * @throws IOException if an error occurred
+	 * This method is called when a form has its tag value method equals to
+	 * post.
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
 	 */
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		response.setCharacterEncoding("utf-8");
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		String name=request.getParameter("name");
-	 	String id=request.getParameter("id");
-	 	String date=request.getParameter("date");
-	 	String note=request.getParameter("note");
+		String name = request.getParameter("name");
+		String id = request.getParameter("id");
+		String date = request.getParameter("date");
+		String note = request.getParameter("note");
+	   
+		
+	/*	String Msg = "{'姓名':'" + name + "','学号':'" + id + "','日期':'" + date
+		+ "','考勤情况':'" + note + "'}";
+        JSONObject data = JSONObject.fromObject(Msg);
+        out.print(data);
+*/
+		
+		List<StuDetails> add=new ArrayList<StuDetails>();
+		add.add(new StuDetails(name,id,date,note));
+		request.setAttribute("StuDetails", add);
+		request.getRequestDispatcher("client.jsp").forward(request, response);
+		
+		
+		
+		
 
-	 	String Msg="{'name':'"+name+"','id':'"+id+"','date':'"+date+"','note':'"+note+"'}";
-
-	 	JSONObject data=JSONObject.fromObject(Msg);
-	 	out.println(data);
+         
 	}
 
 	/**
 	 * Initialization of the servlet. <br>
-	 *
-	 * @throws ServletException if an error occurs
+	 * 
+	 * @throws ServletException
+	 *             if an error occurs
 	 */
 	public void init() throws ServletException {
 		// Put your code here
