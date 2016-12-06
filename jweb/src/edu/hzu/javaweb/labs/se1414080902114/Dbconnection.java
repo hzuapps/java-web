f9a038b4c41c4d83034f58391caf1639c4bfb8bb
ruizhuzhiling <ruizhuzhiling>
2016-12-06 23:40:42 +0800

    #7 #86 提交实验代码

---
+++ jweb/src/edu/hzu/javaweb/labs/se1414080902114/Dbconnection.java
@@ -0,0 +1,53 @@
+package se1414080902114;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Dbconnection {
+	private String dbUrl="jdbc:mysql://localhost:3306/shetuan";
+	private String dbUser="root";
+	private String dbPassword="qwc525431";
+	private String jdbcName="com.mysql.jdbc.Driver";
+	
+	//数据库连接
+	public Connection getConn(){
+		Connection conn = null;
+		try{
+			Class.forName(jdbcName);
+		}
+		catch(Exception e){}
+		try{
+			conn=DriverManager.getConnection(dbUrl,dbUser,dbPassword);
+		}
+		catch(SQLException ex){}
+		return conn;		
+	}
+	
+	//数据库释放
+	public void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
+		if (rs != null)
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		if (pstmt != null)
+			try {
+				pstmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		if (conn != null)
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+	}
+}
