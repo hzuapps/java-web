1a00432b55283a7394250e1c4fde60f8979fe7c1
zoukewen <zoukewen>
2016-12-09 11:12:42 +0800

    #7

---
+++ jweb/src/edu/hzu/javaweb/labs/se1414080902104/DbConnection.java
@@ -0,0 +1,39 @@
+package JDBC;
+ 
+ import java.sql.Connection;
+ import java.sql.DriverManager;
+ import java.sql.PreparedStatement;
+ import java.sql.ResultSet;
+ import java.sql.SQLException;
+ 
+  public class DbConnect {
+  private static String driverName="com.mysql.jdbc.Driver";
+  private static String userName="root";
+  private static String userPwd="root";
+  private static String dbName="students";
+  static Connection getDBConnection(){
+ 	 String url1="jdbc:mysql://localhost:3306/"+dbName;
+ 	 String url2="?user="+userName+"&password="+userPwd;
+ 	 String url3="&userUnicode=true&characterEncoding=utf8&useSSL=true";
+	 String url=url1+url2+url3;
+ 	 try{
+ 		 Class.forName(driverName);
+ 		 Connection con=DriverManager.getConnection(url);
+ 		 return con;
+ 	 }catch(Exception e){
+ 		 e.printStackTrace();
+ 	 }
+ 	return null;
+ 	 
+  }
+  public static void closeDB(Connection con,PreparedStatement pstm,ResultSet rs){
+ 	 try{
+ 		 if(rs!=null)rs.close();
+ 		 if(pstm!=null)pstm.close();
+ 		 if(con!=null)con.close();
+ 	 }catch(SQLException e){
+ 		 e.printStackTrace();
+ 	 }
+  }
+ 		 
+ }
---
+++ jweb/src/edu/hzu/javaweb/labs/se1414080902104/Index.jsp
@@ -0,0 +1,11 @@
+</div>
+  	<div class="row">
+  		<div class="span4" style="float:left;width: 20%" >
+ 			<form class="form-horizontal" id="target" style="height: 250px;margin-top:2%" action="loginServlet" method="get">
+ 			<form class="form-horizontal" id="target" style="height: 250px;margin-top:2%" action="Second.jsp" method="get">
+  				<div class="control-group">
+ 					<label class="control-label" for="inputEmail">ÐÕ&nbsp;Ãû</label>
+ 					<label class="control-label" for="inputEmail">Ñ§&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ºÅ</label>
+  					<div class="controls">
+  						<input id="inputEmail" type="text" name="username" />
+  					</div>
