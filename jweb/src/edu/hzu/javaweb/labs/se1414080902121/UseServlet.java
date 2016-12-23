ba093f092ae17813f6243695e7202fe0adee05dd
yoxiang <18003021640@163.com>
2016-12-23 11:09:40 +0800

    #7 提交代码

---
+++ jweb/src/edu/hzu/javaweb/labs/se1414080902121/UseServlet.java
@@ -0,0 +1,39 @@
+package edu.hzu.javaweb.labs.se1414080902121;
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javabean.DataB;
+public class UseServlet implements javax.servlet.Servlet{
+public void destroy() {
+}
+public ServletConfig getServletConfig() {
+return null;
+}
+public String getServletInfo() {
+return null;
+}
+public void init(ServletConfig arg0) throws ServletException {
+}
+public void doPost(HttpServletRequest request,HttpServletResponse response)
+throws ServletException,IOException{
+String userName = request.getParameter("username");
+String password = request.getParameter("password");
+DataB db = new DataB();
+boolean canLogin = db.loginSuccess(userName, password);
+if(canLogin){
+response.sendRedirect("display.jsp");
+}else{
+response.sendRedirect("failure.jsp");
+}
+}
+public void service(ServletRequest request, ServletResponse response)
+throws ServletException, IOException {
+HttpServletRequest rq = (HttpServletRequest)request;
+HttpServletResponse rs = (HttpServletResponse) response;
+doPost(rq,rs);
+}
+}
